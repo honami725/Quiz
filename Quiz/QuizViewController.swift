beade7bea917e9afa2955a57583e9c1f58da79c7
Honami <honami.725725@gmail.com>
2016-02-10 01:44:11 +0900

    first commit

---
+++ Quiz/QuizViewController.swift
@@ -0,0 +1,93 @@
+//
+//  QuizViewController.swift
+//  Quiz
+//
+//  Created by ohtatomotaka on 2015/02/10.
+//  Copyright (c) 2015年 LifeisTech. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+    //クイズを格納する配列
+    var quizArray = [AnyObject]()
+    
+    //正解数
+    var correctAnswer:Int = 0
+    
+    //クイズを表示するTextView
+    @IBOutlet var quizTextView: UITextView!
+    
+    //選択肢のボタン
+    @IBOutlet var choiceButtons1: UIButton!
+    @IBOutlet var choiceButtons2: UIButton!
+    @IBOutlet var choiceButtons3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var tmpArray = [AnyObject]()
+        
+        //------------------------ここから下にクイズを書く------------------------//
+        tmpArray.append(["女子ラクロスは1チーム何人で構成されて行う競技？","10人","11人","12人",3])
+        tmpArray.append(["ゴールキーパーのことをなんと呼ぶ？","ゴーラー","ゴーリー","ゴールマン",2])
+        tmpArray.append(["ラクロスでプレイヤーが使うスティックのことをなんて呼ぶ？","ラケット","ネット","クロス",3])
+        tmpArray.append(["女子ラクロスでプレイ中に身につけないものはこの中でどれ？","ヘルメット","マウスピース","グローブ",1])
+        tmpArray.append(["日本で一番初めにラクロスチームを結成した大学はどこ？","東京大学","慶應大学","早稲田大学",2])
+        tmpArray.append(["ラクロスが出てくるジブリ作品はどれ？","猫の恩返し","思い出のマーニー","崖の上のポニョ",1])
+        
+        //------------------------ここから上にクイズを書く------------------------//
+        
+        // 問題をシャッフルしてquizArrayに格納する
+        while (tmpArray.count > 0) {
+            let index = Int(arc4random_uniform(UInt32(tmpArray.count)))
+            quizArray.append(tmpArray[index])
+            tmpArray.removeAtIndex(index)
+        }
+        choiceQuiz()
+    }
+    
+    func choiceQuiz() {
+        quizTextView.text = quizArray[0][0] as! String
+        
+        //選択肢のボタンにそれぞれ選択肢のテキストをセット
+        choiceButtons1.setTitle(quizArray[0][1] as? String, forState: .Normal)
+        choiceButtons2.setTitle(quizArray[0][2] as? String, forState: .Normal)
+        choiceButtons3.setTitle(quizArray[0][3] as? String, forState: .Normal)
+    }
+    
+    @IBAction func choiceAnswer(sender: UIButton) {
+        if quizArray[0][4] as! Int == sender.tag {
+            //正解数を増やす
+            correctAnswer++
+        }
+        
+        quizArray.removeAtIndex(0)
+        //解いた問題数の合計が予め設定していた問題数に達したら結果画面へ
+        if quizArray.count == 0 {
+            performSegueToResult()
+        } else {
+            choiceQuiz()
+        }
+    }
+    
+    func performSegueToResult() {
+        performSegueWithIdentifier("toResultView", sender: nil)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if (segue.identifier == "toResultView") {
+            
+            let resultView = segue.destinationViewController as! ResultViewController
+            resultView.correctAnswer = self.correctAnswer
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
+
